cba593b084a8e249e78428b0c4fb3443c5c17d92
Chris Ashman <chrisashman96@gmail.com>
2020-03-17 18:43:54 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,4 @@
-INSERT INTO (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 11, "Male", "Black", "Nice", "Alive")
+INSERT INTO (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 11, "Male", "Black", "Nice", "1");
+INSERT INTO (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 5, "Male", "Brown", "Nice", "1");
+INSERT INTO (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 6, "Female", "Black", "Angry", "1");
+INSERT INTO (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 13, "Male", "Red", "Angry", "0");
